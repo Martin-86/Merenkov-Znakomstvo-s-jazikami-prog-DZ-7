ae8334a636ddfba8c9909052c7b0e79c710671c3
Martin <rauch83@yandex.ru>
2022-08-30 01:48:11 +0300

    2 auf

---
+++ 3x4/Program.cs
@@ -0,0 +1,34 @@
+﻿//              Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//              m = 3, n = 4.
+
+
+
+void Printarray(int[,] arr)
+{
+    int[,] array = new int[3, 4];
+
+    for (int i = 0; i < arr.GetLength(0); i++)   // matrix.GetLength(0) - берет количество строк(0 означает строки)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) // matrix.GetLength(1)- означает столбцы(1)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-10,10);
+        }
+    }
+}
+
+int[,] array = new int[3, 4];
+FillArray(array);
+Printarray(array);
---
+++ el massiva/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int[,] number = {{41,67,96,77},
+                 {14,72,26,71},
+                 {65,52,33,23},};
+
+Console.WriteLine("Введите номер строки ");
+int row = Convert.ToInt32(Console.ReadLine())-1;
+Console.WriteLine("Введите номер столбца ");
+int column = Convert.ToInt32(Console.ReadLine())-1;
+if(row < number.GetLength(0))
+{
+    if(column < number.GetLength(0)+1)
+    {
+        Console.WriteLine(number[row, column]);
+    }
+    else
+    {
+        Console.WriteLine("Нет такого элемента"); 
+    }
+
+}
+else
+{
+Console.WriteLine("Нет такого элемента");
+}
